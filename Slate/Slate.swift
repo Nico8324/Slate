73ae02f3e7957e1bcb6681ee835532a599b8405f
Nico8324 <vanardoisnicolas@icloud.com>
2026-09-04 13:58:56 +0200

    Initial Commit

---
+++ Slate/Slate.swift
@@ -0,0 +1,9 @@
+//
+//  Slate.swift
+//  Slate
+//
+//  Created by Vanardois Nicolas  on 04/09/2026.
+//
+
+import Foundation
+
---
+++ SlateTests/SlateTests.swift
@@ -0,0 +1,19 @@
+//
+//  SlateTests.swift
+//  SlateTests
+//
+//  Created by Vanardois Nicolas  on 04/09/2026.
+//
+
+import Testing
+@testable import Slate
+
+struct SlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
